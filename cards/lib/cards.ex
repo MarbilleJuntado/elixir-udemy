33ceb22f6c78c4de14fb17d85800571171392d48
juanshotman <jm.cabrera@descouvre.com>
2018-02-17 21:25:13 +0800

    Added four main methods to the Cards module

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,5 +1,27 @@
 defmodule Cards do
+  # creates a new deck of cards
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", 
+    "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  # shuffles the deck
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  # returns true if deck contains card
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  # distributes hand_size number of decks given a deck
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 end
